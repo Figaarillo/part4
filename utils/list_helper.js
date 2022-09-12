1d89e15af17ef04e959076efe9a863fffb212ef9
Figarillo <axel.leonardi.22@gmail.com>
2022-09-12 16:55:00 -0300

    activity 4.4 - test total likes function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,14 @@ const dummy = blogs => {
 	return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = blogs => {
+	const totalLikes =
+		blogs.length === 0
+			? 0
+			: blogs.reduce((sum, blog) => {
+					return sum + blog.likes;
+			  }, 0);
+	return totalLikes;
+};
+
+module.exports = { dummy, totalLikes };
