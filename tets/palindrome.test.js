8740de0a5baa980fa4bd7fa193deaaadfff513b6
Figarillo <axel.leonardi.22@gmail.com>
2022-09-12 10:54:52 -0300

    activity 4.3 - my firts test

---
+++ tets/palindrome.test.js
@@ -0,0 +1,19 @@
+const palindrome = require('../utils/for_testing').palindrome;
+
+test('palindrome of neuquen', () => {
+	const result = palindrome('neuquen');
+
+	expect(result).toBe('neuquen');
+});
+
+test('palindrome of react', () => {
+	const result = palindrome('react');
+
+	expect(result).toBe('tcaer');
+});
+
+test('palindrome of releveler', () => {
+	const result = palindrome('releveler');
+
+	expect(result).toBe('releveler');
+});
